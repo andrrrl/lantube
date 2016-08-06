804fff664a019d71b3e1acd5e1d4a9c495d20b4f
Andrrr <andresin@gmail.com>
2016-08-06 18:28:43 -0300

    removed unused JS file 2

--- public/js/lantube.js
+++
@@ -1,1 +0,0 @@
-$(function(){console.log("Hola mundo."),$("#agregar").on("click",function(o){o.preventDefault(),$.ajax({url:"/videos",method:"post",data:{url:$("#video").val(),order:$(".orden").length+1||1},success:function(o){console.log("Si!"),$("#video").val(""),$(".lista-videos").append('<tr><td class="orden">'+o.order+"</td><td>"+o.url+"</td></tr>")}})})});
