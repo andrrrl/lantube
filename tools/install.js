6bb669a9dabef536d442e8ba16cb600d4fcc5785
Daniela <andresin@gmail.com>
2016-08-18 13:58:14 -0300

    Improved postinstall script

--- tools/install.js
+++ tools/install.js
@@ -3,12 +3,13 @@
 /**
  *
  * Lantube post install script
+ * by Andrrr <andresin@gmail.com>
  *
- * . This script is to be executed with `npm install`
+ * . This script is meant to be executed with `npm install`
  * 
  * - Will check if a .env file exists
- * 		1) If .env file already exists, just end the script  
- * 		2) If no .env file found, will copy .env_example to .env
+ * 		1) If .env file already exists, will just end the script  
+ * 		2) If no .env file found, will copy .env_example to .env without example comments
  * 
  */
 
@@ -17,25 +18,56 @@ let shell = require('shelljs');
 
 shell.echo('[INFO] Installing Lantube...'.bold);
 
+let wd = shell.pwd().stdout;
+wd = wd.slice(wd.lastIndexOf('/'));
+
+if ( wd === '/tools' && process.argv[2] !== 'test' ) {
+	shell.echo('[ERR] Don\'t run this script standalone.'.red.bold);
+	shell.echo('[ERR] Use `npm install` from root directory instead.'.red.bold);
+	shell.exit(1);
+}
+
+// bool
 let env_found = shell.test('-f', '.env');
 
 if ( env_found ) {
-	shell.echo('[OK] .env file exists'.green);
-	shell.echo('[OK] Lantube installed successfully!'.green)
-	shell.echo('[INFO] Run `gulp lantube` from Lantube\'s root dir'.bold)
+	
+	shell.echo('[OK] .env file exists'.green.bold);
+	shell.echo('[OK] Lantube installed successfully!'.green.bold);
+	shell.echo('[INFO] Run `gulp lantube` from Lantube\'s root dir'.bold);
+	
 } else {
-	shell.echo('[WARN] No .env file found, creating default...'.yellow.bold);
-	shell.cp('.env_example', '.env');
-	shell.echo('[OK] .env file created!'.green.bold)
+	
+	shell.echo('[WARN] No .env file found, trying to generate default...'.yellow.bold);
+	
+	// do we have an .env_example file?
+	let env_example_found = shell.test('-f', '.env_example');
+	
+	if ( env_example_found ) {
+		shell.echo('[INFO] .env_example file found.'.bold);
+		
+		// Create our brand new .env file!
+		var env_created = shell.cp('.env_example', '.env');
+	
+		if ( env_created ){
+			shell.echo('[INFO] Generating file...'.bold);
+			
+			// Remove example comments
+			shell.exec('cat .env_example | grep -vP \'(\# \\(?\\!)\' > .env', { silent:true }, function(code, stdout, stderr){
+				shell.echo('[OK] .env file created!'.green.bold);
+				shell.echo('[INFO] You can delete .env_example now.'.bold);
+				shell.echo('[INFO] Don\'t forget to check your config in .env file!'.bold);
+				shell.exit(code);
+			});
+			
+			
+		} else {
+			shell.echo('[ERR] Cannot create .env file'.red.bold);
+		}
+		
+	} else {
+		shell.echo('[ERR] .env_example file not found. See README.md for more info.'.red.bold);
+		shell.exit(1);
+	}
+	
 }
-
-shell.echo('[INFO] Don\'t forget to config your .env file!'.bold);
-
-let env_example_found = shell.test('-f', '.env_example');
-
-if ( env_example_found) {
-	shell.echo('[INFO] You can delete .env_example now.'.bold);
-}
-
-shell.echo('Bye!'.green);
-shell.exit(0);
