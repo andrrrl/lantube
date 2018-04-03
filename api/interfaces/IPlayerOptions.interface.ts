9d2a3f56dfd14dcacd338e670a1d2a83950dcb90
Andrrr <andresin@gmail.com>
2018-04-02 22:32:44 -0300

    Added Player Options interface

---
+++ api/interfaces/IPlayerOptions.interface.ts
@@ -0,0 +1,11 @@
+export interface IPlayerOptions {
+    player: string;
+    playerMode: string;
+    _id: string;
+    title: string;
+    url: string;
+    img: string;
+    status: string;
+    playlist: boolean;
+    list: any[];
+}
