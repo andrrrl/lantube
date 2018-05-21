129994c3d5ab8c4f4ef3b1c952e8405a5cc9cab5
Andrrr <andresin@gmail.com>
2018-05-21 19:53:38 -0300

    Added yt search route

--- api/routes/redis/videos.ts
+++ api/routes/redis/videos.ts
@@ -1,19 +1,20 @@
 import * as express from 'express';
 import * as request from 'request';
 import { Videos } from './../../controllers/redis/videos';
+import { Search } from './../../controllers/redis/search';
 
 let router = express.Router();
 
 export = (io) => {
 
     let VideosCtrl = new Videos(io);
+    let SearchCtrl = new Search();
 
     router.route('/api/videos')
 
         .get(async (req, res, next) => {
             let videos = await VideosCtrl.getAll('videos');
             res.json(videos);
-
         });
 
     router.route('/api/videos/add/:video')
@@ -40,15 +41,17 @@ export = (io) => {
 
         });
 
-
     router.route('/api/videos/delete/:videoId')
         .delete(async (req, res, next) => {
-
             let deleted = await VideosCtrl.delete(req.params.videoId);
             res.json(deleted);
-
         });
 
+    router.route('/api/videos/search/:term')
+        .get(async (req, res, next) => {
+            let videos = await SearchCtrl.search(req.params.term);
+            res.json(videos);
+        });
 
     return router;
 
