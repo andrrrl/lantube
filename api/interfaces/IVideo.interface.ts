03f7eabe1c47da3d3e751f0b3c9e4ab972faa37d
Andrés <andresin@gmail.com>
2018-12-11 17:08:29 -0300

    Updated IVideo to fit stats

--- api/interfaces/IVideo.interface.ts
+++ api/interfaces/IVideo.interface.ts
@@ -1,6 +1,8 @@
 export interface IVideo {
     videoId: string;
-    title: string;
-    url: string;
-    img: string;
+    videoInfo: {
+        title: string;
+        url: string;
+        img: string;
+    }
 }
