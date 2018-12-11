e70cdeae657179c3a478063489e232de4ef074b3
Andrés <andresin@gmail.com>
2018-12-11 17:06:33 -0300

    Continuous play functionality (playlist)

--- api/controllers/redis/player.ts
+++ api/controllers/redis/player.ts
@@ -4,8 +4,8 @@ import * as mongoose from 'mongoose';
 import * as fs from 'fs';
 import * as ServerSchema from '../../schemas/redis/Server';
 import { Videos } from './../../controllers/redis/videos';
-import { IPlayerOptions } from './../../interfaces/IPlayerOptions.interface';
 import { IPlayerStats } from "../../interfaces/IPlayerStats";
+import { IVideo } from "../../interfaces/IVideo.interface";
 
 const
     exec = ChildProcess.exec;
@@ -18,17 +18,14 @@ process.on('unhandledRejection', r => console.log(r));
 
 export class Player {
 
-    formats: string = ' -f 34/18/43/35/44/22/45/37/46';
-    playerOptions: IPlayerOptions;
-
-    currentStatus: any = 'idle';
-    volumeChange: any;
-
-    videosCtrl: any;
-
     private playing: ChildProcess.ChildProcess;
-    private chromecast: Buffer;
+
+    formats: string = ' -f 34/18/43/35/44/22/45/37/46';
     playerStats: IPlayerStats;
+
+    volumeChange: any;
+    videosCtrl: any;
+    stopped: any;
 
     constructor(private io: any) {
         this.io = io;
@@ -37,7 +34,6 @@ export class Player {
         Server.getPlayerStats().then(playerStats => {
             this.playerStats = playerStats;
         });
-
     };
 
     /**
@@ -48,105 +44,51 @@ export class Player {
         return mongoose.Types.ObjectId.isValid(id);
     }
 
-
     // Only for omxplayer for now
     pause() {
         return new Promise(async (resolve, reject) => {
-            if (process.env.PLAYER !== 'omxplayer') {
-                if (process.env.PLAYER !== 'cvlc -I cli') {
-                    console.info('Pause not supported!');
-                } else {
-                    this.playing.stdin.write("pause\n");
-                }
+
+            if (this.playing && this.playing.pid) {
+                this.playing.stdin.write(" ");
             } else {
-                if (this.playing && this.playing.pid) {
-                    this.playing.stdin.write("p");
-                } else {
-                    resolve(false);
-                }
+                resolve(false);
             }
 
-            console.info('Playback paused!');
+            let current = await Server.getPlayerStats();
+
+            let status: any = (current.status === 'paused' || current.status === 'stopped') ? 'playing' : 'paused';
+
+            console.info(`Playback ${(current.status === 'paused' || current.status === 'stopped') ? 'starting' : 'paused'}!`);
+
+            let playerOptions: IPlayerStats = {
+                videoId: current.videoId,
+                videoInfo: current.videoInfo,
+                status,
+                playlist: current.playlist,
+            };
+            playerOptions.videoId = current.videoId;
+            playerOptions.videoInfo = current.videoInfo;
+
+            if (status === 'playing') {
+                this.playNext(playerOptions);
+            }
 
             // Update stats
-            let stats = {
+            let stats: IPlayerStats = {
                 player: process.env.PLAYER,
-                status: 'paused',
-                videoId: this.playerOptions.videoId,
+                status: status,
+                videoId: current.videoId,
+                videoInfo: current.videoInfo,
+                playlist: current.playlist,
                 lastUpdated: new Date(),
             };
 
-            await Server.setPlayerStats(stats);
+            Server.setPlayerStats(stats);
 
             this.io.emit('PLAYER_MESSAGE', stats);
 
             resolve(true);
         })
-    }
-
-    // If videoId is null, let's consider 'video1' as starting ponit
-    getVideoOrder(videoId = 'video0') {
-        return Number(videoId.replace('video', ''));
-    }
-
-    playPrev(playerOptions) {
-        return new Promise(async (resolve, reject) => {
-
-            let order = this.getVideoOrder(playerOptions.videoId);
-
-            let prevVideo = await this.videosCtrl.getPrev('videos', order);
-
-            this.playerOptions = prevVideo;
-
-            // console.log('prev', this.playerOptions);
-            this.play(this.playerOptions).then(result => {
-
-                // Update stats
-                let stats = {
-                    player: process.env.PLAYER,
-                    status: 'playing',
-                    videoId: this.playerOptions.videoId,
-                    lastUpdated: new Date(),
-                };
-
-                Server.setPlayerStats(stats);
-                this.io.emit('PLAYER_MESSAGE', stats);
-                resolve(this.playerOptions);
-            });
-
-        });
-    }
-
-
-    playNext(playerOptions) {
-        return new Promise(async (resolve, reject) => {
-
-            playerOptions = await Server.getPlayerStats();
-            console.log({ playerOptions });
-            let order = this.getVideoOrder(playerOptions.videoId);
-
-            let nextVideo = await this.videosCtrl.getNext('videos', order);
-
-            this.playerOptions = nextVideo;
-
-            console.log('next', this.playerOptions);
-
-            this.play(this.playerOptions).then(result => {
-
-                // Update stats
-                let stats = {
-                    player: process.env.PLAYER,
-                    status: 'playing',
-                    videoId: this.playerOptions.videoId,
-                    lastUpdated: new Date(),
-                };
-
-                Server.setPlayerStats(stats);
-                this.io.emit('PLAYER_MESSAGE', stats);
-                resolve(this.playerOptions);
-            });
-        });
-
     }
 
     // Only for omxplayer for now
@@ -158,7 +100,7 @@ export class Player {
                 this.volumeChange = '+';
             }
             if (process.env.PLAYER !== 'omxplayer') {
-                console.info('Volumen change not supported!');
+                console.info('Volume change not supported!');
                 reject('Volume change not supported!')
             } else {
                 this.playing.stdin.write(this.volumeChange);
@@ -174,120 +116,167 @@ export class Player {
 
     stopAll() {
         return new Promise(async (resolve, reject) => {
-
-            // Update stats
-            let stats = {
-                player: process.env.PLAYER,
-                status: 'stopped',
-                videoId: this.playerStats.videoId,
-                lastUpdated: new Date(),
-            };
-            this.io.emit('PLAYER_MESSAGE', stats);
-            Server.setPlayerStats(stats);
-            
-            if (process.env.PLAYER !== 'omxplayer') {
-                if (process.env.PLAYER !== 'cvlc -I cli') {
+            if (this.playing && this.playing.pid) {
+                if (this.playing.stdin.writable) {
+                    this.playing.stdin.write("q");
                     if (!this.playing.killed) {
-                        this.playing.kill('SIGINT');
-                        // kill(this.playing.pid, 'SIGINT');
-                    }
-                } else {
-                    this.playing.stdin.write("stop\n");
-                    this.playing.stdin.write('clear\n');
-                }
-                console.log(process.env.PLAYER, 'STOP (stop button)');
-            } else {
-                if (this.playing && this.playing.pid) {
-                    if (this.playing.stdin.writable) {
-                        this.playing.stdin.write("q");
-                        if (!this.playing.killed) {
-                            this.playing.kill();
+                        this.playing.kill();
+                        if (this.playing.killed) {
+                            resolve(true);
                         }
-                        resolve(true);
-                    } else {
-                        resolve(false);
                     }
                 } else {
                     resolve(true);
                 }
             }
+
             this.playing = null;
             resolve(true);
+        });
+    }
 
+    // If videoId is null, let's consider 'video1' as starting ponit
+    getVideoOrder(videoId = 'video') {
+        return Number(videoId.replace('video', ''));
+    }
+
+    playPrev(playerOptions) {
+        return new Promise(async (resolve, reject) => {
+
+            let order = this.getVideoOrder(playerOptions.videoId);
+            let prevVideo: IVideo = await this.videosCtrl.getPrev('videos', order);
+
+            this.playerStats = await Server.getPlayerStats();
+
+            playerOptions.videoId = prevVideo.videoId;
+            playerOptions.videoInfo = prevVideo.videoInfo;
+
+            this.playerStats.videoId = playerOptions.videoInfo.videoId;
+            this.playerStats.videoInfo = playerOptions.videoInfo;
+
+            this.play(this.playerStats).then(result => {
+                // Update stats
+                let stats: IPlayerStats = {
+                    player: process.env.PLAYER,
+                    status: 'playing',
+                    videoId: this.playerStats.videoId,
+                    videoInfo: this.playerStats.videoInfo,
+                    playlist: this.playerStats.playlist,
+                    lastUpdated: new Date(),
+                };
+
+                Server.setPlayerStats(stats);
+                this.io.emit('PLAYER_MESSAGE', stats);
+                resolve(this.playerStats);
+            });
 
         });
     }
 
 
-    play(playerOptions) {
+    playNext(playerOptions) {
         return new Promise(async (resolve, reject) => {
 
-            this.playerOptions = playerOptions;
+            this.playerStats = await Server.getPlayerStats();
+            playerOptions.playlist = this.playerStats.playlist;
 
-            let player = playerOptions.player || process.env.PLAYER;
-            var videoUrl = playerOptions.url || '';
+            let order = this.getVideoOrder(this.playerStats.videoInfo.videoId);
+            let nextVideo: IVideo = await this.videosCtrl.getNext('videos', order);
 
-            let player_playlist = this.playerOptions.playlist === true ? process.env.PLAYER_PLAYLIST : process.env.PLAYER_NO_PLAYLIST;
-            let playerMode = this.playerOptions.playerMode || process.env.PLAYER_MODE || 'windowed';
+            playerOptions.videoId = nextVideo.videoId;
+            playerOptions.videoInfo = nextVideo.videoInfo;
 
-            let playerModeArg = '';
+            this.playerStats.videoId = playerOptions.videoInfo.videoId;
+            this.playerStats.videoInfo = playerOptions.videoInfo;
 
-            // Player mode?
-            switch (playerMode) {
-                case 'windowed':
-                    playerModeArg = process.env.PLAYER_NO_PLAYLIST;
-                    break;
-                case 'fullscreen':
-                    playerModeArg = process.env.PLAYER_MODE_FULLSCREEN_ARG;
-                    break;
-                case 'audio-only':
-                    playerModeArg = process.env.PLAYER_MODE_AUDIO_ONLY_ARG;
-                    break;
-                case 'chromecast':
-                    this.chromecast = execSync(process.env.YOUTUBE_DL + ' -o - ' + videoUrl + ' | castnow --quiet -');
-                    break;
-            }
+
+            this.play(playerOptions).then(result => {
+
+                // Update stats
+                let stats: IPlayerStats = {
+                    player: process.env.PLAYER,
+                    status: 'playing',
+                    videoId: this.playerStats.videoInfo.videoId,
+                    videoInfo: this.playerStats.videoInfo,
+                    playlist: this.playerStats.playlist,
+                    lastUpdated: new Date(),
+                };
+
+                Server.setPlayerStats(stats);
+                this.io.emit('PLAYER_MESSAGE', stats);
+                resolve(this.playerStats);
+            });
+        });
+
+    }
+
+    async play(playerOptions) {
+        return new Promise(async (resolve, reject) => {
+
+            this.playerStats = await Server.getPlayerStats();
+            this.playerStats.status = playerOptions.status;
+            this.playerStats.videoId = playerOptions.videoInfo.videoId;
+            this.playerStats.videoInfo = playerOptions.videoInfo;
+
+            var videoUrl = playerOptions.videoInfo.url;
 
             // Stop/clear any current playback before starting
-            let stopped = await this.stopAll();
+            this.stopped = await this.stopAll();
 
-            if (stopped) {
+            // Update stats
+            let stats: IPlayerStats = {
+                player: process.env.PLAYER,
+                status: 'loading',
+                videoId: this.playerStats.videoInfo.videoId,
+                videoInfo: this.playerStats.videoInfo,
+                playlist: this.playerStats.playlist,
+                lastUpdated: new Date()
+            };
+
+            this.io.emit('PLAYER_MESSAGE', stats);
+
+            if (this.stopped) {
                 // Player type?
                 if (process.env.PLAYER === 'omxplayer') {
                     if (this.playing === null) {
+
+                        console.log('Extracting Youtube URL...');
+                        let youtubeURL = await this.extracYoutubeURL(videoUrl);
+
+                        console.log('Starting OMXPLAYER...');
+                        this.playing = await this.startPlayer(youtubeURL);
+
+                        this.initPlaybackSession();
+
                         // Update stats
-                        let stats = {
+                        stats = {
                             player: process.env.PLAYER,
                             status: 'playing',
-                            videoId: this.playerOptions.videoId,
+                            videoId: this.playerStats.videoInfo.videoId,
+                            videoInfo: this.playerStats.videoInfo,
+                            playlist: this.playerStats.playlist,
                             lastUpdated: new Date(),
                         };
 
-                        console.log('exctracting youtube URL...');
-                        let youtubeURL = await this.extracYoutubeURL(videoUrl);
-                        console.log('starting omxplayer...');
-                        await this.startPlayer(youtubeURL);
-                        await Server.setPlayerStats(stats);
+                        this.playerStats = stats;
+
                         this.io.emit('PLAYER_MESSAGE', stats);
 
-                        this.initPlaybackSession(videoUrl);
-                        resolve(this.playerOptions);
-                    }
-                } else {
-                    if (player_playlist) {
-                        this.playlist(this.playerOptions.list);
-                        player_playlist = '/tmp/playlist.pls';
+                        Server.setPlayerStats(stats);
+
+                        resolve(this.playerStats);
                     }
                 }
 
-            } else {
-                console.log('WARNING, can\'t stop the beat I can\'t stop. Retrying...');
-                this.play(this.playerOptions);
+            }
+            else {
+                // console.log('WARNING, can\'t stop the beat I can\'t stop.');
+                await this.play(this.playerStats);
             }
 
         }).catch(async result => {
-            console.log('ERROR, can\'t stop the beat I can\'t stop. Retrying...');
-            await this.play(this.playerOptions);
+            // console.log('ERROR, can\'t stop the beat I can\'t stop.');
+            // await this.play(this.playerOptions);
         });
 
     }
@@ -302,90 +291,53 @@ export class Player {
         });
     }
 
-    startPlayer(playableURL): Promise<boolean> {
+    startPlayer(playableURL): Promise<ChildProcess.ChildProcess> {
         return new Promise((resolve, reject) => {
             // OMXPLAYER won't pipe anything to stdout, only to stderr if option -I or --info is used
             // --alpha 0 
             this.playing = exec(`${process.env.PLAYER} -b -o both --vol -1200 --threshold 30 --audio_fifo 30 -I "${playableURL}"`);
             this.playing.stderr.once('data', (data) => {
-                resolve(true);
+                resolve(this.playing);
             });
         });
 
     }
 
-    async initPlaybackSession(videoUrl) {
+    async initPlaybackSession() {
 
-        if (process.env.PLAYER === 'cvlc -I cli' && (typeof this.playing !== 'undefined')) {
-            let ytb = execSync('youtube-dl -g ' + videoUrl);
-            this.playing.stdin.write('add ' + ytb.toString().split('\n')[1], () => {
-                this.playing.stdin.write('stop\n', () => {
-                    console.log(process.env.PLAYER, 'STOP (cleanup)');
-                    this.playing.stdin.write('clear\n', () => {
-                        this.playing.stdin.write('play\n', () => {
-                            console.log(process.env.PLAYER, 'PLAY (cleanup)');
-                        });
-                    });
-                });
-            });
-        }
+        this.playerStats = await Server.getPlayerStats();
 
-        // this.playing.stdout.on('data', (data) => {
+        this.playing.on('disconnect', () => {
 
-        //     if (process.env.NODE_ENV === 'development') {
-        //         console.log('Starting playback with ' + (JSON.stringify(this.playerOptions) || 'no options.'));
-        //         return true;
-        //     }
-        //     return false;
-        //     // console.log(`stdout: ${data}`);
-
-        //     // Check if connection is stuck (for now only mpv / mplayer)
-        //     // this.retryPlayback(data);
-        // });
-
-        // this.playing.stderr.on('data', data => {
-        //     if (process.env.NODE_ENV === 'development') {
-        //         // will print stuff continuously...
-        //     }
-        //     // console.log(`stderr: ${data}`);
-        // });
+        });
 
         this.playing.on('exit', code => {
+            this.stopped = true;
             if (process.env.NODE_ENV === 'development') {
                 console.log(`Player exited with code ${code}`);
             }
-            if (this.playerOptions.playlist === true) {
-                this.playNext(this.playerOptions);
-            }
         });
 
-        // this.playing.on('disconnect', code => {
-        //     if (process.env.NODE_ENV === 'development') {
-        //         console.log(`Player disconnected with code ${code}`);
-        //     }
-
-        // });
-
-        // Close when video finished (I don't want a playlist, understand?)
+        // Player is closed when video finishes
         this.playing.on('close', code => {
-            if (process.env.NODE_ENV === 'development') {
-                // console.log(`Player finshed playing with code ${code}`);
-            }
+            this.stopped = true;
 
-            if (process.env.PLAYER === 'cvlc -I cli') {
-                this.playing.stdin.write("stop\n");
-                this.playing.kill('SIGINT');
+            if (this.playerStats.playlist === true) {
+                this.playNext(this.playerStats);
             }
 
             // Update stats
-            let stats = {
+            let stats: IPlayerStats = {
                 player: process.env.PLAYER,
                 status: 'stopped',
-                videoId: this.playerOptions.videoId,
+                videoId: this.playerStats.videoInfo.videoId,
+                videoInfo: this.playerStats.videoInfo,
+                playlist: this.playerStats.playlist,
                 lastUpdated: new Date(),
             };
 
             this.io.emit('PLAYER_MESSAGE', stats);
+
             // Update player stats
             Server.setPlayerStats(stats);
 
