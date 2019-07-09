c1e1d1984c515fa10072759f79ac1b827ccd13f9
Andrés <andresin@gmail.com>
2019-07-09 16:38:12 -0300

    Lint

--- tools/install.js
+++ tools/install.js
@@ -15,9 +15,9 @@
 
 require('colors');
 
-const 
+const
 	shell = require('shelljs'),
- 	env_example = '.env.example';
+	env_example = '.env.example';
 
 
 shell.echo('           ╭─────────────────────────╮           '.bold.yellow);
@@ -29,7 +29,7 @@ shell.echo(' [INFO] Installing Lantube...'.bold);
 let wd = shell.pwd().stdout;
 wd = wd.slice(wd.lastIndexOf('/'));
 
-if ( wd === '/tools' && process.argv[2] !== 'test' ) {
+if (wd === '/tools' && process.argv[2] !== 'test') {
 	shell.echo(' [ERR] Cannot run this script standalone.'.red.bold);
 	shell.echo(' [ERR] Use `npm install` from root directory instead.'.red.bold);
 	shell.exit(1);
@@ -39,44 +39,44 @@ if ( wd === '/tools' && process.argv[2] !== 'test' ) {
 // bool
 let env_found = shell.test('-f', '.env');
 
-if ( env_found ) {
-	
+if (env_found) {
+
 	shell.echo(' [OK] .env file exists'.green.bold);
 	shell.echo(' [OK] Lantube installed successfully!'.green.bold);
-	shell.echo(' [INFO] Run `gulp lantube` or `gulp` (for dev) from Lantube\'s root dir'.bold);
-	
+	shell.echo(' [INFO] Run `node app.js` or `npm start` (for dev) from Lantube\'s root dir'.bold);
+
 } else {
-	
+
 	shell.echo(' [WARN] No .env file found, trying to generate default...'.yellow.bold);
-	
+
 	// do we have an .env_example file?
 	let env_example_found = shell.test('-f', env_example);
-	
-	if ( env_example_found ) {
+
+	if (env_example_found) {
 		shell.echo((' [OK] ' + env_example + ' file found.').bold.green);
-		
+
 		// Create our brand new .env file!
 		var env_created = shell.cp(env_example, '.env');
-	
-		if ( env_created ){
+
+		if (env_created) {
 			shell.echo(' [INFO] Generating...'.bold);
-			
+
 			// Remove example comments
-			shell.exec('cat ' + env_example + ' | grep -vP \'(\# \\(?\\!)\' > .env', { silent:true }, function(code, stdout, stderr){
+			shell.exec('cat ' + env_example + ' | grep -vP \'(\# \\(?\\!)\' > .env', { silent: true }, function (code, stdout, stderr) {
 				shell.echo(' [OK] .env file created!'.green.bold);
 				shell.echo((' [INFO] You can delete ' + env_example + ' now.').bold);
 				shell.echo(' [INFO] Don\'t forget to check your config in .env file!'.bold);
 				shell.exit(code);
 			});
-			
-			
+
+
 		} else {
 			shell.echo(' [ERR] Cannot create .env file'.red.bold);
 		}
-		
+
 	} else {
 		shell.echo((' [ERR] ' + env_example + ' file not found. \nSee README.md for more info.').red.bold);
 		shell.exit(1);
 	}
-	
+
 }
