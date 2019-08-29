69d475119479562f81964941c99c9aa531468d55
Andrés <andresin@gmail.com>
2019-08-29 20:47:52 -0300

    feat (dht sensor): added route

---
+++ api/routes/sensor/dht.ts
@@ -0,0 +1,19 @@
+import * as express from 'express';
+import { DHT11 } from '../../controllers/sensor/dht';
+
+let router = express.Router();
+
+export = (io) => {
+
+    let dhtCtrl = new DHT11(io);
+
+    router.route('/api/sensor/dht')
+
+        .get(async (req, res, next) => {
+            let dht = await dhtCtrl.initSensor();
+            res.json(dht);
+        });
+
+    return router;
+
+}
