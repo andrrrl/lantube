71d8f97d03cc3be45c15eccdc7ad48cea4a8cd45
andrrrl <andresin@gmail.com>
2019-09-06 00:29:25 -0300

    feat (coreTemp): use relay to start cpu fan cooler

--- api/controllers/sensor/coreTemp.ts
+++ api/controllers/sensor/coreTemp.ts
@@ -1,42 +1,75 @@
 import { Socket } from 'net';
 import { ChildProcess, exec } from 'child_process';
+import * as relayCtrl from '../relay/relay';
 
 export class CoreTemp {
     thermal: ChildProcess;
+    relayStarted = false;
+    relay;
+    temperature: number;
+
 
     constructor(private io: Socket) {
         this.io = io;
-    };
+        this.relay = new relayCtrl.Relay();
 
-    readSensor() {
-        console.log('Leyendo datos del sensor thermal temp...')
-        setTimeout(() => {
-            this.thermal = null;
-            this.thermal = exec('cat /sys/class/thermal/thermal_zone0/temp');;
-        }, 300000);
-    }
+        setInterval(() => {
 
-    initSensor() {
-        return new Promise(async (resolve, reject) => {
+            this.thermal = exec('cat /sys/class/thermal/thermal_zone0/temp');
+            console.log('Leyendo temperatura del core...')
+            this.thermal.stdout.once('data', (data) => {
+                console.log('stdout');
 
-            this.thermal = exec('cat /sys/class/thermal/thermal_zone0/temp');;
-            this.readSensor();
+                this.temperature = Math.round(Number(data) / 1000);
 
-            this.thermal.stdout.on('data', (data) => {
+                console.log(this.temperature);
+
                 const coreTemp = {
                     sensor: 'coreTemp',
-                    value: `${Math.round(Number(data) / 1000)}°C`
+                    value: `${this.temperature}°C`
                 }
-                console.log('Emitiendo....');
+
+                this.sendCoreTemp();
+
+                console.log('emit temp');
                 this.io.emit('SENSOR_MESSAGE', coreTemp);
 
-                resolve({
-                    sensor: {
-                        name: 'thermal_zone0',
-                        dataTypes: ['temperatura'],
-                        value: coreTemp
-                    }
-                });
+            });
+
+            this.thermal.once('exit', () => {
+                console.log('exit');
+
+                if (!this.relayStarted && this.temperature > 60) {
+                    this.relay.relayON();
+                    this.relayStarted = true;
+                }
+
+                if (this.relayStarted && this.temperature <= 58) {
+                    this.relay.relayOFF();
+                    this.relayStarted = false;
+                }
+
+                this.thermal = null;
+
+            });
+        }, 10000);
+
+        process.on('SIGINT', () => {
+            console.log('Relay shutdown');
+            this.relay.unexport();
+        });
+
+    };
+
+    sendCoreTemp() {
+        return new Promise(async (resolve, reject) => {
+            console.log('send temp');
+            return resolve({
+                sensor: {
+                    name: 'thermal_zone0',
+                    dataTypes: ['temperatura'],
+                    value: this.temperature
+                }
             });
 
         });
